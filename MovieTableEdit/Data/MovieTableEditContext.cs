9db2f458b6e2604a23961cb6eb678b230200033f
YasuThompson <yasutotamura6161@gmail.com>
2024-09-12 15:06:22 +0200

    Adjusted data models and dbcontext

--- MovieTableEdit/Data/MovieTableEditContext.cs
+++ MovieTableEdit/Data/MovieTableEditContext.cs
@@ -21,81 +21,111 @@ namespace MovieTableEdit.Data
             optionsBuilder.UseSqlite(Configuration.GetConnectionString("MovieTableEditContext"));
         }
 
-        public DbSet<MockMovieFact> MockMovieFact { get; set; }
-        public DbSet<MockMovieFactDE> MockMovieFactDE { get; set; }
-        public DbSet<MockMovieFactFR> MockMovieFactFR { get; set; }
-        public DbSet<MockMovieDim> MockMovieDim { get; set; }
+        public DbSet<MockMoviePriceDE> MockMoviePriceDE { get; set; }
+        public DbSet<MockTVPriceDE> MockTVPriceDE { get; set; }
+        public DbSet<MockAnimePriceDE> MockAnimePriceDE { get; set; }
+        public DbSet<MockComicPriceDE> MockComicPriceDE { get; set; }
+
+        public DbSet<MockMoviePriceFR> MockMoviePriceFR { get; set; }
+        public DbSet<MockTVPriceFR> MockTVPriceFR { get; set; }
+        public DbSet<MockAnimePriceFR> MockAnimePriceFR { get; set; }
+        public DbSet<MockComicPriceFR> MockComicPriceFR { get; set; }
+
+        public DbSet<MockProductDim> MockProductDim { get; set; }
         public DbSet<MockGenreDim> MockGenreDim { get; set; }
+        public DbSet<MockProductTypeDim> MockProductTypeDim { get; set; }
+        public DbSet<MockRatingDEDim> MockRatingDEDim { get; set; }
+        public DbSet<MockRatingFRDim> MockRatingFRDim { get; set; }
 
-
-        List<MockMovieFact> mock_movie_facts = new List<MockMovieFact>{
-            new MockMovieFact
-            {   
-                Id = 1, 
-                MovieId = 1111, 
-                Date = new DateOnly(1979, 4, 12),
-                Price = 2.51M,
-                
-            },
-            new MockMovieFact
-            {   
-                Id = 2, 
-                MovieId = 1112, 
-                Date = new DateOnly(1981, 12, 24),
-                Price = 2.78M,
-            },
-            new MockMovieFact
-            {   
-                Id = 3, 
-                MovieId = 1113,
-                Date = new DateOnly(1985, 7, 10),
-                Price = 3.55M,
-            },
-            new MockMovieFact
-            {
-                Id = 4, 
-                MovieId = 1114, 
-                Date = new DateOnly(2015, 5, 15),
-                Price = 8.43M,
-            },
-            new MockMovieFact
-            {
-                Id = 5, 
-                MovieId = 1115, 
-                Date = new DateOnly(2024, 5, 24),
-                Price = 13.49M,
-            }
-        };
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {   
+            // TODO: should the processes below generalised and written in a for loop?
+            // TODO: Naming rules might not be coherent
 
-            string mock_de_csv_path = "Data/MockMovieFactDE.csv";
-            List<MockMovieFactDE> mock_movie_facts_de = ReadCsvAsClassList<MockMovieFactDE>(mock_de_csv_path);
+            string mock_movie_price_de_csv_path = "Data/MockMoviePriceDE.csv";
+            List<MockMoviePriceDE> mock_movie_price_de = ReadCsvAsClassList<MockMoviePriceDE>(mock_movie_price_de_csv_path);
 
-            string mock_fr_csv_path = "Data/MockMovieFactFR.csv";
-            List<MockMovieFactFR> mock_movie_facts_fr = ReadCsvAsClassList<MockMovieFactFR>(mock_fr_csv_path);
+            string mock_tv_price_de_csv_path = "Data/MockTVPriceDE.csv";
+            List<MockTVPriceDE> mock_tv_price_de = ReadCsvAsClassList<MockTVPriceDE>(mock_tv_price_de_csv_path);
 
-            string mock_movie_dim_csv_path = "Data/MockMovieDim.csv";
-            List<MockMovieDim> mock_movie_dims = ReadCsvAsClassList<MockMovieDim>(mock_movie_dim_csv_path);
+            string mock_anime_price_de_csv_path = "Data/MockAnimePriceDE.csv";
+            List<MockAnimePriceDE> mock_anime_price_de = ReadCsvAsClassList<MockAnimePriceDE>(mock_anime_price_de_csv_path);
+
+            string mock_comic_price_de_csv_path = "Data/MockComicPriceDE.csv";
+            List<MockComicPriceDE> mock_comic_price_de = ReadCsvAsClassList<MockComicPriceDE>(mock_comic_price_de_csv_path);
+
+
+            string mock_movie_price_fr_csv_path = "Data/MockMoviePriceFR.csv";
+            List<MockMoviePriceFR> mock_movie_price_fr = ReadCsvAsClassList<MockMoviePriceFR>(mock_movie_price_fr_csv_path);
+
+            string mock_tv_price_fr_csv_path = "Data/MockTVPriceFR.csv";
+            List<MockTVPriceFR> mock_tv_price_fr = ReadCsvAsClassList<MockTVPriceFR>(mock_tv_price_fr_csv_path);
+
+            string mock_anime_price_fr_csv_path = "Data/MockAnimePriceFR.csv";
+            List<MockAnimePriceFR> mock_anime_price_fr = ReadCsvAsClassList<MockAnimePriceFR>(mock_anime_price_fr_csv_path);
+
+            string mock_comic_price_fr_csv_path = "Data/MockComicPriceFR.csv";
+            List<MockComicPriceFR> mock_comic_price_fr = ReadCsvAsClassList<MockComicPriceFR>(mock_comic_price_fr_csv_path);
+
+
+            string mock_product_dim_csv_path = "Data/MockProductDim.csv";
+            List<MockProductDim> mock_product_dims = ReadCsvAsClassList<MockProductDim>(mock_product_dim_csv_path);
 
             string mock_genre_dim_csv_path = "Data/MockGenreDim.csv";
             List<MockGenreDim> mock_genre_dims = ReadCsvAsClassList<MockGenreDim>(mock_genre_dim_csv_path);
 
-            modelBuilder.Entity<MockMovieFact>().ToTable("MockMovieFact");
-            modelBuilder.Entity<MockMovieFact>().HasData(mock_movie_facts);
+            string mock_product_type_dim_csv_path = "Data/MockProductTypeDim.csv";
+            List<MockProductTypeDim> mock_product_type_dims = ReadCsvAsClassList<MockProductTypeDim>(mock_product_type_dim_csv_path);
 
-            modelBuilder.Entity<MockMovieFactDE>().ToTable("MockMovieFactDE");
-            modelBuilder.Entity<MockMovieFactDE>().HasData(mock_movie_facts_de);
+            string mock_rating_de_dim_csv_path = "Data/MockRatingDEDim.csv";
+            List<MockRatingDEDim> mock_rating_de_dims = ReadCsvAsClassList<MockRatingDEDim>(mock_rating_de_dim_csv_path);
 
-            modelBuilder.Entity<MockMovieFactFR>().ToTable("MockMovieFactFR");
-            modelBuilder.Entity<MockMovieFactFR>().HasData(mock_movie_facts_fr);
+            string mock_rating_fr_dim_csv_path = "Data/MockRatingFRDim.csv";
+            List<MockRatingFRDim> mock_rating_fr_dims = ReadCsvAsClassList<MockRatingFRDim>(mock_rating_fr_dim_csv_path);
 
-            modelBuilder.Entity<MockMovieDim>().ToTable("MockMovieDim");
-            modelBuilder.Entity<MockMovieDim>().HasData(mock_movie_dims);
+
+            modelBuilder.Entity<MockMoviePriceDE>().ToTable("MockMoviePriceDE");
+            modelBuilder.Entity<MockMoviePriceDE>().HasData(mock_movie_price_de);
+
+            modelBuilder.Entity<MockTVPriceDE>().ToTable("MockTVPriceDE");
+            modelBuilder.Entity<MockTVPriceDE>().HasData(mock_tv_price_de);
+
+            modelBuilder.Entity<MockAnimePriceDE>().ToTable("MockAnimePriceDE");
+            modelBuilder.Entity<MockAnimePriceDE>().HasData(mock_anime_price_de);
+
+            modelBuilder.Entity<MockComicPriceDE>().ToTable("MockComicPriceDE");
+            modelBuilder.Entity<MockComicPriceDE>().HasData(mock_comic_price_de);
+
+
+            modelBuilder.Entity<MockMoviePriceFR>().ToTable("MockMoviePriceFR");
+            modelBuilder.Entity<MockMoviePriceFR>().HasData(mock_movie_price_fr);
+
+            modelBuilder.Entity<MockTVPriceFR>().ToTable("MockTVPriceFR");
+            modelBuilder.Entity<MockTVPriceFR>().HasData(mock_tv_price_fr);
+
+            modelBuilder.Entity<MockAnimePriceFR>().ToTable("MockAnimePriceFR");
+            modelBuilder.Entity<MockAnimePriceFR>().HasData(mock_anime_price_fr);
+
+            modelBuilder.Entity<MockComicPriceFR>().ToTable("MockComicPriceFR");
+            modelBuilder.Entity<MockComicPriceFR>().HasData(mock_comic_price_fr);
+
+
+            modelBuilder.Entity<MockProductDim>().ToTable("MockProductDim");
+            modelBuilder.Entity<MockProductDim>().HasData(mock_product_dims);
+
+            modelBuilder.Entity<MockProductTypeDim>().ToTable("MockProductTypeDim");
+            modelBuilder.Entity<MockProductTypeDim>().HasData(mock_product_type_dims);
 
             modelBuilder.Entity<MockGenreDim>().ToTable("MockGenreDim");
             modelBuilder.Entity<MockGenreDim>().HasData(mock_genre_dims);
+
+            modelBuilder.Entity<MockRatingDEDim>().ToTable("MockRatingDEDim");
+            modelBuilder.Entity<MockRatingDEDim>().HasData(mock_rating_de_dims);
+
+            modelBuilder.Entity<MockRatingFRDim>().ToTable("MockRatingFRDim");
+            modelBuilder.Entity<MockRatingFRDim>().HasData(mock_rating_fr_dims);
+
         }
 
 
--- MovieTableEdit/Models/Movie.cs
+++ MovieTableEdit/Models/Movie.cs
@@ -3,20 +3,64 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MovieTableEdit.Models;
 
-public class MockMovieFact
+
+public class MockMoviePriceFR
 {
     public int Id { get; set;}
-    public int MovieId { get; set; }
+    public int ProductId { get; set; }
 
-    public DateOnly Date { get; set; }
+    public DateOnly Timestamp { get; set; }
 
     [DataType(DataType.Currency)]
     [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
+
+    public int? RatingFR {get; set; }
 }
 
+public class MockTVPriceFR
+{
+    public int Id { get; set;}
+    public int ProductId { get; set; }
 
-public class MockMovieFactFR
+    public DateOnly Timestamp { get; set; }
+
+    [DataType(DataType.Currency)]
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal Price { get; set; }
+
+    public int? RatingFR {get; set; }
+}
+
+public class MockAnimePriceFR
+{
+    public int Id { get; set;}
+    public int ProductId { get; set; }
+
+    public DateOnly Timestamp { get; set; }
+
+    [DataType(DataType.Currency)]
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal Price { get; set; }
+
+    public int? RatingFR {get; set; }
+}
+
+public class MockComicPriceFR
+{
+    public int Id { get; set;}
+    public int ProductId { get; set; }
+
+    public DateOnly Timestamp { get; set; }
+
+    [DataType(DataType.Currency)]
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal Price { get; set; }
+
+    public int? RatingFR {get; set; }
+}
+
+public class MockMoviePriceDE
 {
     public int Id { get; set;}
     public int MovieId { get; set; }
@@ -27,13 +71,14 @@ public class MockMovieFactFR
     [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
 
-    public string? RatingFR {get; set; }
+    public int? RatingDE {get; set; }
+
 }
 
-public class MockMovieFactDE
+public class MockTVPriceDE
 {
     public int Id { get; set;}
-    public int MovieId { get; set; }
+    public int ProductId { get; set; }
 
     public DateOnly Timestamp { get; set; }
 
@@ -41,15 +86,43 @@ public class MockMovieFactDE
     [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
 
-    public string? RatingDE {get; set; }
-
+    public int? RatingDE {get; set; }
 }
 
-public class MockMovieDim
+public class MockAnimePriceDE
+{
+    public int Id { get; set;}
+    public int ProductId { get; set; }
+
+    public DateOnly Timestamp { get; set; }
+
+    [DataType(DataType.Currency)]
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal Price { get; set; }
+
+    public int? RatingDE {get; set; }
+}
+
+public class MockComicPriceDE
+{
+    public int Id { get; set;}
+    public int ProductId { get; set; }
+
+    public DateOnly Timestamp { get; set; }
+
+    [DataType(DataType.Currency)]
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal Price { get; set; }
+
+    public int? RatingDE {get; set; }
+}
+
+public class MockProductDim
 {   
     [Key]
-    public int MovieId { get; set; }
+    public int ProductId { get; set; }
     public string Title { get; set; }
+    public int ProductTypeId { get; set;}
     public int GenreId { get; set;}
     public string? TitleDE { get; set; }
     public string? TitleFR { get; set; }
@@ -64,4 +137,25 @@ public class MockGenreDim
     [Key]
     public int GenreId { get; set; }
     public string Genre { get; set; }
+}
+
+public class MockProductTypeDim
+{
+    [Key]
+    public int ProductTypeId { get; set; }
+    public string ProductType { get; set; }
+}
+
+public class MockRatingDEDim
+{
+    [Key]
+    public int RatingDEId { get; set; }
+    public string RatingDE { get; set; }
+}
+
+public class MockRatingFRDim
+{
+    [Key]
+    public int RatingFRId { get; set; }
+    public string RatingFR { get; set; }
 }
