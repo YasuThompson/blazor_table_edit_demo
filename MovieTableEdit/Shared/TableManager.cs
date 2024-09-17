2195c9da15a227fdc23f2ee40e76e557ddf171e3
YasuThompson <yasutotamura6161@gmail.com>
2024-09-17 19:02:35 +0200

    Adjusted a namespace

--- MovieTableEdit/Shared/TableManager.cs
+++ MovieTableEdit/Shared/TableManager.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 
 
-namespace MovieTableEdit.TableManager
+namespace MovieTableEdit.Shared.TableManager
 {
     public class TableIndexManager
     {
