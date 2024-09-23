19821d4f144650d4b8f16fabde178a01460f08a9
YasuThompson <yasutotamura6161@gmail.com>
2024-09-23 20:05:28 +0200

    Removed all "required" fields from models

--- MovieTableEdit/Models/Dimension.cs
+++ MovieTableEdit/Models/Dimension.cs
@@ -7,45 +7,45 @@ namespace MovieTableEdit.Models;
 public class TempProductDim
 {   
     [Key]
-    public required int ProductId { get; set; }
-    public required string Title { get; set; }
-    public required int ProductTypeId { get; set;}
-    public required int GenreId { get; set;}
-    public required string TitleDE { get; set; }
-    public required string TitleFR { get; set; }
-    public required string TitleES { get; set; }
-    public required string TitleIT { get; set; }
-    public required string TitleJP { get; set; }
-    public required string Note {get; set;}
+    public int ProductId { get; set; }
+    public string Title { get; set; }
+    public int ProductTypeId { get; set;}
+    public int GenreId { get; set;}
+    public string TitleDE { get; set; }
+    public string TitleFR { get; set; }
+    public string TitleES { get; set; }
+    public string TitleIT { get; set; }
+    public string TitleJP { get; set; }
+    public string Note {get; set;}
 }
 
 public class TempGenreDim
 {
     [Key]
-    public required int GenreId { get; set; }
-    public required string Genre { get; set; }
+    public int GenreId { get; set; }
+    public string Genre { get; set; }
     public string? Explanation { get; set; }
 }
 
 public class TempProductTypeDim
 {
     [Key]
-    public required int ProductTypeId { get; set; }
-    public required string ProductType { get; set; }
+    public int ProductTypeId { get; set; }
+    public string ProductType { get; set; }
 }
 
 public class TempRatingDEDim
 {
     [Key]
-    public required int RatingDEId { get; set; }
-    public required string RatingDE { get; set; }
+    public int RatingDEId { get; set; }
+    public string RatingDE { get; set; }
 }
 
 public class TempRatingFRDim
 {
     [Key]
-    public required int RatingFRId { get; set; }
-    public required string RatingFR { get; set; }
+    public int RatingFRId { get; set; }
+    public string RatingFR { get; set; }
 }
 
 
@@ -55,43 +55,43 @@ public class TempRatingFRDim
 public class ProductDim
 {   
     public int Id { get; set;}
-    public required int ProductId { get; set; }
-    public required string Title { get; set; }
-    public required int ProductTypeId { get; set;}
-    public required int GenreId { get; set;}
-    public required string TitleDE { get; set; }
-    public required string TitleFR { get; set; }
-    public required string TitleES { get; set; }
-    public required string TitleIT { get; set; }
-    public required string TitleJP { get; set; }
-    public required string Note {get; set;}
+    public int ProductId { get; set; }
+    public string Title { get; set; }
+    public int ProductTypeId { get; set;}
+    public int GenreId { get; set;}
+    public string TitleDE { get; set; }
+    public string TitleFR { get; set; }
+    public string TitleES { get; set; }
+    public string TitleIT { get; set; }
+    public string TitleJP { get; set; }
+    public string Note {get; set;}
 }
 
 public class GenreDim
 {
     public int Id { get; set;}
-    public required int GenreId { get; set; }
-    public required string Genre { get; set; }
+    public int GenreId { get; set; }
+    public string Genre { get; set; }
     public string? Explanation { get; set; }
 }
 
 public class ProductTypeDim
 {
     public int Id { get; set;}
-    public required int ProductTypeId { get; set; }
-    public required string ProductType { get; set; }
+    public int ProductTypeId { get; set; }
+    public string ProductType { get; set; }
 }
 
 public class RatingDEDim
 {
     public int Id { get; set;}
-    public required int RatingDEId { get; set; }
-    public required string RatingDE { get; set; }
+    public int RatingDEId { get; set; }
+    public string RatingDE { get; set; }
 }
 
 public class RatingFRDim
 {
     public int Id { get; set;}
-    public required int RatingFRId { get; set; }
-    public required string RatingFR { get; set; }
+    public int RatingFRId { get; set; }
+    public string RatingFR { get; set; }
 }
